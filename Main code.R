c6f3769acbd7399361c7769a5a367a97f07d6619
elisethai <elise.thai@psl.eu>
2023-06-12 13:59:20 +0200

    Year max

--- Main code.R
+++ Main code.R
@@ -2,6 +2,7 @@ library(gpinter)
 data<-read.csv("Povcalnet 2017.csv")
 View(data)
 
+#CONSIGNES
 # Pour tous pays récupérer la dernière année des donnée sdispo 
 # estimer taux crissance  entre cette année kla et 2030 (cf tx croissance observée sur BM en $ 2017 PPP)
 # imputer une croissance entre maintennat et 2030 (soit trouver une institiution tq BM ou GMS qui le fait soit prendre la croissance observée dans les 5 dernières années observ&es hors covid pour avoir un tx de recalibrage avec lequel on va multiplier le revenu total du pays)
@@ -11,5 +12,10 @@ View(data)
 # calculer: trouver valeur au delà de laquelle il faut tout expropier pour combler pgap avec transferts internes
 # autre indicateurs: au delà de 2,15$ impôt linéaire; quel tx appliquer pour combler pvgap et regarder tx linéaire de 2,15`$, 6,95$, 13$`
 
-#coucou
-# hshskkhs
+install.packages ("tidyverse")
+library(tidyverse)
+data <- read.csv("Povcalnet 2017.csv")
+temp <- data %>% group_by(country_code) %>% summarize(year_max= max(year))
+year_max <- setNames(temp$year_max, temp$country_code)
+data$year_max <- year_max[data$country_code]
+data <- data[data$year == data$year_max,]
