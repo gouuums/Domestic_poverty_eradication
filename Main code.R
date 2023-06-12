857451393f1e8510127afbe2bf6b3ebdbea0553e
gouuums <t.goumont@hotmail.fr>
2023-06-12 12:03:33 +0200

    initial commit

---
+++ Main code.R
@@ -0,0 +1,12 @@
+ibrary(gpinter)
+data<-read.csv("Povcalnet 2017.csv")
+View(data)
+
+# Pour tous pays récupérer la dernière année des donnée sdispo 
+# estimer taux crissance  entre cette année kla et 2030 (cf tx croissance observée sur BM en $ 2017 PPP)
+# imputer une croissance entre maintennat et 2030 (soit trouver une institiution tq BM ou GMS qui le fait soit prendre la croissance observée dans les 5 dernières années observ&es hors covid pour avoir un tx de recalibrage avec lequel on va multiplier le revenu total du pays)
+# reproduire le travail du papier
+# regarder le poverty gap à 2,15$ lvl indiv puis pays l'exprimer en $/personne (poverty  gap moyen) 
+# faire une Hi=g entre 2030 va être la même pour chaque personne dans chaque pays
+# calculer: trouver valeur au delà de laquelle il faut tout expropier pour combler pgap avec transferts internes
+# autre indicateurs: au delà de 2,15$ impôt linéaire; quel tx appliquer pour combler pvgap et regarder tx linéaire de 2,15`$, 6,95$, 13$`
