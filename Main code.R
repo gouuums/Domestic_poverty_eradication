5abdb47e096e64cb704063900acbe566b43588a7
elisethai <elise.thai@psl.eu>
2023-06-13 11:57:53 +0200

    Tableaux des différentes variables par percentile + organisation

--- Main code.R
+++ Main code.R
@@ -12,7 +12,7 @@ View(data)
 # calculer: trouver valeur au delà de laquelle il faut tout expropier pour combler pgap avec transferts internes
 # autre indicateurs: au delà de 2,15$ impôt linéaire; quel tx appliquer pour combler pvgap et regarder tx linéaire de 2,15`$, 6,95$, 13$`
 
-
+#Récupération des données
 library(tidyverse)
 data <- read.csv("Povcalnet 2017.csv")
 temp <- data %>% group_by(country_code) %>% summarize(year_max= max(year))
@@ -21,35 +21,7 @@ data$year_max <- year_max[data$country_code]
 data <- data[data$year == data$year_max,]
 View(data)
 
-data_pivot <- data %>%
-  pivot_wider(names_from = percentile, values_from = avg_welfare)
-
-str(data_pivot)
-
-data_pivot <- data %>%
-  filter(!is.na(avg_welfare)) %>%
-  group_by(country_code, percentile) %>%
-  summarize(mean_avg_welfare = mean(avg_welfare))
-str(data_pivot)
-data_wide <- data_pivot %>%
-  pivot_wider(names_from = percentile, values_from = mean_avg_welfare)
-
-str(data_wide)
-data_pivot %>%
-  pivot_wider(names_from = percentile, values_from = mean_avg_welfare, values_fill = 0) %>%
-  head()
-view(data_wide)
-
-chiffresmoyenne <- names(data_wide)[-1]
-moyenne <- rowMeans(data_wide[,chiffresmoyenne])
-
-print(Croissance_pays[-c(1), ])
-
-colnames(Croissance_pays)[1:2:3:4:5:6:7:8:9] <- c("country_code","Growth rate 2014", "Growth rate 2015","Growth rate 2016", "Growth rate 2017", "Growth rate 2018","Growth rate 2019","Growth rate 2020","Growth rate 2021")
-print("Renamed Croissance_pays : ")
-print(Croissance_pays)
-View(Croissance_pays)
-
+#Tableau de l'average welfare par percentile
 data_pivot <- data %>%
   filter(!is.na(avg_welfare)) %>%
   group_by(country_code, percentile) %>%
@@ -57,11 +29,67 @@ data_pivot <- data %>%
 str(data_pivot)
 data_avgwelf <- data_pivot %>%
   pivot_wider(names_from = percentile, values_from = mean_avg_welfare)
-
 str(data_avgwelf)
 data_pivot %>%
   pivot_wider(names_from = percentile, values_from = mean_avg_welfare, values_fill = 0) %>%
   head()
 View(data_avgwelf)
+
+#Tableau du welfare share par percentile
+data_pivot <- data %>%
+  filter(!is.na(welfare_share)) %>%
+  group_by(country_code, percentile) %>%
+  summarize(mean_welfare_share = mean(welfare_share))
+str(data_pivot)
+data_welfshare <- data_pivot %>%
+  pivot_wider(names_from = percentile, values_from = mean_welfare_share)
+str(data_welfshare)
+data_pivot %>%
+  pivot_wider(names_from = percentile, values_from = mean_welfare_share, values_fill = 0) %>%
+  head()
+View(data_welfshare)
+
+#Tableau de la pop share par percentile
+data_pivot <- data %>%
+  filter(!is.na(pop_share)) %>%
+  group_by(country_code, percentile) %>%
+  summarize(mean_pop_share = mean(pop_share))
+str(data_pivot)
+data_pop_share <- data_pivot %>%
+  pivot_wider(names_from = percentile, values_from = mean_pop_share)
+str(data_pop_share)
+data_pivot %>%
+  pivot_wider(names_from = percentile, values_from = mean_pop_share, values_fill = 0) %>%
+  head()
+View(data_pop_share)
+
+#Tableau du quantile par percentile
+data_pivot <- data %>%
+  filter(!is.na(quantile)) %>%
+  group_by(country_code, percentile) %>%
+  summarize(mean_quantile = mean(quantile))
+str(data_pivot)
+data_quantile <- data_pivot %>%
+  pivot_wider(names_from = percentile, values_from = mean_quantile)
+str(data_quantile)
+data_pivot %>%
+  pivot_wider(names_from = percentile, values_from = mean_quantile, values_fill = 0) %>%
+  head()
+View(data_quantile)
+
+#En cours de travail
+lignes_a_supprimer <- data$Colonne %in% tableau1 & !data$Colonne %in% tableau2
+
+#Tableau du welfare type par percentile: Pb car pivot-wider ne fonctionne pas pour des mots
+
+#Moyenne: à refaire comme on n'utilise plus xata_wide
+#chiffresmoyenne <- names(data_wide)[-1]
+#moyenne <- rowMeans(data_wide[,chiffresmoyenne])
+
+colnames(Croissance_pays)[1:2:3:4:5:6:7:8:9] <- c("country_code","Growth rate 2014", "Growth rate 2015","Growth rate 2016", "Growth rate 2017", "Growth rate 2018","Growth rate 2019","Growth rate 2020","Growth rate 2021")
+print("Renamed Croissance_pays : ")
+print(Croissance_pays)
+View(Croissance_pays)
+
 
 
