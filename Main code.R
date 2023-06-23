a49d435e280de059965c1fa2800e0af80b0c2a57
bixiou <fabre.adrien@hotmail.fr>
2023-06-23 12:42:49 +0200

    idea

--- Main code.R
+++ Main code.R
@@ -7,9 +7,11 @@
 # imputer un croissance entre maintennat et 2030 (soit trouver une institiution tq BM ou GMS qui le fait soit prendre la croissance observée dans les 5 dernières années observ&es hors covid pour avoir un tx de recalibrage avec lequel on va multiplier le revenu total du pays)
 # reproduire le travail du papier
 # regarder le poverty gap à 2,15$ lvl indiv puis pays l'exprimer en $/personne (poverty  gap moyen) 
-# faire une Hi=g entre 2030 va être la même pour chaque personne daNs chaque pays
+# faire une Hy=g entre 2030 va être la même pour chaque personne daNs chaque pays
 # calculer: trouver valeur au delà de laquelle il faut tout expropier pour combler pgap avec transferts internes
 # autre indicateurs: au delà de 2,15$ impôt linéaire; quel tx appliquer pour combler pvgap et regarder tx linéaire de 2,15`$, 6,95$, 13$`
+# calculer Gini sans redistribution, et avec redistribution linéaire vs. expropriative => indicateur: réduction de Gini nécessaire pour éradiquer pauvreté.
+# autre indicateur: quel y minimum on peut assurer en expropriant tout au-delà de $13/day? $7?
 
 
 # Data fetch
